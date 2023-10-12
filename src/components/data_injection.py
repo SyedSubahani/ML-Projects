b5543b526bd4418fe0dbfab9400cdd63a5e0befd
Syed <syed.subahani@gmail.com>
2023-10-13 00:58:08 +0530

    Trasnformation pipline

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+#from src.components.model_trainer import ModelTrainerConfig
+#from src.components.model_trainer import ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -36,7 +40,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Injection of the data is completed")
 
             return (
                 self.ingestion_config.train_data_path
@@ -50,3 +54,9 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    #modeltrainer=ModelTrainer()
+    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
+from dataclasses import dataclass
 
 @dataclass
 class DataTransformationConfig:
